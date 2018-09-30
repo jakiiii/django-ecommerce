87355f0e7d70d0e2ed0752fc35f493d708bdf7bb
jakiiii <me.jaki@outlook.com>
2018-09-30 20:41:47 +0600

    create cart function in view.py

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import settings
+
+from products.models import Product
+
+USER = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class CartManager(models.Manager):
+    def new_cart_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
+    def new(self, user=None):
+        user_obj = None
+        if user is not None and user.is_authenticated:
+            user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
+# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(USER, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(max_digits=50, decimal_places=2, default=0.00)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
+
+    def __str__(self):
+        return str(self.id)
