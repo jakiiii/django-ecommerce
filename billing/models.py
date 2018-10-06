b2c31ab4fdc8c5c1fe25c61b6a47ba0bbe11ff81
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-07 02:15:14 +0600

    add BillingProfileManager in billing app

--- billing/models.py
+++ billing/models.py
@@ -2,7 +2,27 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 
+from accounts.models import GuestEmail
+
 User = settings.AUTH_USER_MODEL
+
+
+# Create your manager here.
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if request.user.is_authenticated:
+            'login in user checkout; remember payment staff.'
+            obj, created = self.model.objects.get_or_create(user=request.user, email=request.user.email)
+        elif guest_email_id is not None:
+            'guest user checkout; auto reload payment staff.'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(email=guest_email_obj.email)
+        else:
+            print('error check out home in login or guest email!')
+        return obj, created
 
 
 # Create your models here.
@@ -12,6 +32,8 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BillingProfileManager()
 
     def __str__(self):
         return self.email
