08bcd650b385f22339e803be519893da2c667a9b
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-08 04:21:55 +0600

    set reuse address for checkout

--- address/urls.py
+++ address/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import checkout_address_create_view
+from .views import (
+    checkout_address_create_view,
+    checkout_address_reuse_view,
+)
 
 urlpatterns = [
-    path('checkout/address/create/', checkout_address_create_view, name='checkout-address')
+    path('checkout/address/create/', checkout_address_create_view, name='checkout-address'),
+    path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout-reuse')
 ]
--- carts/views.py
+++ carts/views.py
@@ -54,7 +54,10 @@ def checkout_home(request):
     shipping_address_id = request.session.get("shipping_address_id", None)
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    address_qs = None
     if billing_profile is not None:
+        if request.user.is_authenticated:
+            address_qs = Address.objects.filter(billing_profile=billing_profile)
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
         if shipping_address_id:
             order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
@@ -80,5 +83,6 @@ def checkout_home(request):
         "login_form": login_form,
         "guest_form": guest_form,
         "address_form": address_form,
+        "address_qs": address_qs,
     }
     return render(request, template_name, context)
