bd46af98b7e22a8c47fa79e71464121c09afb4a9
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-16 00:40:39 +0600

    ajax - ajax search

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+    // Ajax Search
+    let searchForm = $(".search-form");
+    let searchInput = searchForm.find("[name='q']");
+    let typingTimer;
+    let typingInterval = 1500;  // .5s
+
+    let searchBtn = searchForm.find("[name='submit']");
+
+    searchInput.keyup(function (event) {
+        // key realised
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(performSearch, typingInterval)
+    });
+
+    searchInput.keydown(function (event) {
+        // key pressed
+        clearTimeout(typingTimer);
+    });
+    
+    function searching() {
+        searchBtn.addClass("disabled");
+        searchBtn.html("<i class='fa fa-spin fa-spinner'></i>");
+    }
+
+    function performSearch() {
+        searching();
+        let query = searchInput.val();
+        setTimeout(function () {
+            window.location.href='/search/?q=' + query
+        }, 500)
+    }
+
+
+
+    // Cart Add / Remove Products
     var productForm = $('.form-product-ajax');
 
     productForm.submit(function (event) {
