aa8c7b721d62b4e0789bb5b859e5da4848380213
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-22 04:20:48 +0600

    payment method view with template

--- billing/urls.py
+++ billing/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from billing.views import payment_method_view
+
 
 urlpatterns = [
-
+    path('billing/payment-method/', payment_method_view, name="payment-method")
 ]
--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,16 @@
+import stripe
 from django.shortcuts import render
 
+STRIPE_PUB_KEY = "pk_test_PzZbTHlXOfKISCnJOn0edmlI"
+stripe.api_key = "sk_test_75kkoW8Uu4p38LCZGzyKZ0bB"
+
+
 # Create your views here.
+def payment_method_view(request):
+    template_name = "billing/payment-method.html"
+    context = {
+        "publish_key": STRIPE_PUB_KEY
+    }
+    if request.method == "POST":
+        print(request.method)
+    return render(request, template_name, context)
